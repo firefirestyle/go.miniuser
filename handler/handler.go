65423c3c408f51d15869bc37379d5d133e0e72a8
kyorohiro <kyorohiro@gmail.com>
2016-10-30 15:11:37 +0900

    add blob

--- handler/handler.go
+++ handler/handler.go
@@ -4,31 +4,67 @@ import (
 
 	//	"strings"
 
+	"errors"
+	"net/http"
+	"strings"
+
+	miniblob "github.com/firefirestyle/go.miniblob/blob"
+	blobhandler "github.com/firefirestyle/go.miniblob/handler"
 	"github.com/firefirestyle/go.minipointer"
+	"github.com/firefirestyle/go.miniprop"
 	"github.com/firefirestyle/go.minisession"
 	miniuser "github.com/firefirestyle/go.miniuser/user"
 	"golang.org/x/net/context"
+	"google.golang.org/appengine"
 	"google.golang.org/appengine/log"
 )
 
 type UserHandler struct {
-	manager    *miniuser.UserManager
-	relayIdMgr *minipointer.PointerManager
-	sessionMgr *minisession.SessionManager
+	manager     *miniuser.UserManager
+	relayIdMgr  *minipointer.PointerManager
+	sessionMgr  *minisession.SessionManager
+	blobHandler *blobhandler.BlobHandler
 }
 
 type UserHandlerManagerConfig struct {
-	ProjectId   string
-	UserKind    string
-	RelayIdKind string
-	SessionKind string
+	ProjectId       string
+	UserKind        string
+	RelayIdKind     string
+	SessionKind     string
+	BlobKind        string
+	BlobPointerKind string
+	BlobSign        string
 }
 
 type UserHandlerOnEvent struct {
+	blobOnEvent blobhandler.BlobHandlerOnEvent
 }
 
-func NewUserHandler(config UserHandlerManagerConfig, onEvents UserHandlerOnEvent) *UserHandler {
-	return &UserHandler{
+func NewUserHandler(callbackUrl string, config UserHandlerManagerConfig, onEvents UserHandlerOnEvent) *UserHandler {
+	if config.ProjectId == "" {
+		config.ProjectId = "ffstyle"
+	}
+	if config.UserKind == "" {
+		config.UserKind = "ffuser"
+	}
+	if config.RelayIdKind == "" {
+		config.UserKind = "ffuser-pointer"
+	}
+	if config.SessionKind == "" {
+		config.SessionKind = "ffuser-session"
+	}
+	if config.BlobKind == "" {
+		config.BlobKind = "ffuser-blob"
+	}
+	if config.BlobPointerKind == "" {
+		config.BlobPointerKind = config.BlobKind + "-pointer"
+	}
+	if config.BlobSign == "" {
+		config.BlobSign = miniprop.MakeRandomId()
+	}
+	//
+
+	ret := &UserHandler{
 		manager: miniuser.NewUserManager(miniuser.UserManagerConfig{
 			ProjectId:       config.ProjectId,
 			UserKind:        config.UserKind,
@@ -43,7 +79,35 @@ func NewUserHandler(config UserHandlerManagerConfig, onEvents UserHandlerOnEvent
 			Kind:      config.SessionKind,
 			ProjectId: config.ProjectId,
 		}),
+		//		blobHandler: blobHandlerObj,
 	}
+	completeFunc := onEvents.blobOnEvent.OnBlobComplete
+	onEvents.blobOnEvent.OnBlobComplete = func(w http.ResponseWriter, r *http.Request, outputProp *miniprop.MiniProp, hh *blobhandler.BlobHandler, blobObj *miniblob.BlobItem) error {
+		dir := r.URL.Query().Get("dir")
+		if true == strings.HasPrefix(dir, "/user") {
+			ctx := appengine.NewContext(r)
+			userName := strings.Replace(dir, "/user/", "", -1)
+			userMgrObj := ret
+			userObj, userErr := userMgrObj.GetManager().GetUserFromRelayId(ctx, userName)
+			if userErr != nil {
+				outputProp.SetString("error", "not found user")
+				return userErr
+			}
+			userObj.SetIconUrl("key://" + blobObj.GetBlobKey())
+			userMgrObj.GetManager().SaveUserWithImmutable(ctx, userObj)
+			return completeFunc(w, r, outputProp, hh, blobObj)
+		} else {
+			return errors.New("unsupport")
+		}
+	}
+
+	ret.blobHandler = blobhandler.NewBlobHandler(callbackUrl, config.BlobSign, miniblob.BlobManagerConfig{
+		ProjectId:   config.ProjectId,
+		Kind:        config.BlobKind,
+		PointerKind: config.BlobPointerKind,
+		CallbackUrl: callbackUrl,
+	}, blobhandler.BlobHandlerOnEvent{})
+	return ret
 }
 
 func (obj *UserHandler) GetSessionMgr() *minisession.SessionManager {
---
+++ handler/handler_blob.go
@@ -0,0 +1,63 @@
+package handler
+
+import (
+	"net/http"
+
+	"strings"
+
+	"google.golang.org/appengine"
+)
+
+func (obj *UserHandler) GetArticleIdFromDir(dir string) string {
+	if false == strings.HasPrefix(dir, "/user/") {
+		return ""
+	}
+	t1 := strings.Replace(dir, "/user/", "", 1)
+	t2 := strings.Index(t1, "/")
+	if t2 == -1 {
+		t2 = len(t1)
+	}
+
+	return t1[0:t2]
+}
+
+func (obj *UserHandler) GetDirFromDir(dir string) string {
+	if false == strings.HasPrefix(dir, "/user/") {
+		return ""
+	}
+	t1 := strings.Replace(dir, "/user/", "", 1)
+	t2 := strings.Index(t1, "/")
+	if t2 == -1 {
+		t2 = 0
+	}
+
+	return t1[t2:len(t1)]
+}
+
+func (obj *UserHandler) HandleBlobRequestToken(w http.ResponseWriter, r *http.Request) {
+	//
+	// load param from json
+	articleId := r.URL.Query().Get("userName")
+	dir := r.URL.Query().Get("dir")
+	name := r.URL.Query().Get("file")
+	//
+	// todo check articleId
+
+	//
+	//
+	obj.blobHandler.HandleBlobRequestTokenFromParams(w, r, "/user/"+articleId+"/"+dir, name)
+}
+
+func (obj *UserHandler) HandleBlobUpdated(w http.ResponseWriter, r *http.Request) {
+	//
+	ctx := appengine.NewContext(r)
+	Debug(ctx, "callbeck AAAA")
+	obj.blobHandler.HandleUploaded(w, r)
+}
+
+func (obj *UserHandler) HandleBlobGet(w http.ResponseWriter, r *http.Request) {
+	//
+	ctx := appengine.NewContext(r)
+	Debug(ctx, "callbeck AAAA")
+	obj.blobHandler.HandleGet(w, r)
+}
